77af7f06a8290ae1a93e568405b82877fff39a7a
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-26 20:31:16 +0800

    Added post components to LatestPosts

--- src/components/LatestPosts.jsx
+++ src/components/LatestPosts.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Post from "./Post";
 
 const LatestPosts = () => {
   return (
     <div>
       <h3>Latest Posts</h3>
+      <div className="post-container grid grid-cols-4">
+        <Post postTitle="New Year's eve" date="Jun 18" />
+        <Post postTitle="New Year's eve" date="August 18" />
+        <Post postTitle="New Year's eve" date="May 23" />
+        <Post postTitle="New Year's eve" date="November 12" />
+      </div>
     </div>
   );
 };
