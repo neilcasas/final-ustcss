74b4eecf3bec3194f03d5a036b83e29b81d4a2e3
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-26 23:45:44 +0800

    Fixed bug where subjects did not change based on specialization due to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-const Button = ({ text }) => {
+const Button = ({ text, onClick }) => {
   return (
-    <button className="text-lg font-bold w-auto h-[41px] p-3 rounded-lg text-white bg-accentpurple hover:drop-shadow-xl active:bg-accentorange flex items-center justify-center transition duration-300 ease-in-out">
+    <button
+      className="text-lg font-bold w-auto h-[41px] p-3 rounded-lg text-white bg-accentpurple hover:drop-shadow-xl active:bg-accentorange flex items-center justify-center transition duration-300 ease-in-out"
+      onClick={onClick}
+    >
       {text}
     </button>
   );
