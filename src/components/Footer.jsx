cb0f256db70bf72f7e92749282351621b9055822
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 01:54:25 +0800

    Added UST logo on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import cicsLogo from "../assets/cics.png";
+import ustLogo from "../assets/ust.png";
 import envelopeIcon from "../assets/envelope-solid.svg";
 import facebookIcon from "../assets/facebook.svg";
 import twitterIcon from "../assets/twitter.svg";
@@ -9,11 +10,14 @@ const Footer = () => {
   return (
     <div className="flex h-[211px] w-auto px-24 py-10 justify-between bg-primary text-white">
       <div className="left-half flex flex-col justify-center items-center">
-        <img
-          className="css-logo flex h-[40px] w-fit"
-          src={cicsLogo}
-          alt="css logo"
-        ></img>
+        <div className="flex gap-10">
+          <img className="h-[40px] w-auto" src={ustLogo}></img>
+          <img
+            className="css-logo flex h-[40px] w-fit"
+            src={cicsLogo}
+            alt="css logo"
+          ></img>
+        </div>
         <p className="text-center text-xs my-2">
           University of Santo Tomas College of Computing Sciences
         </p>
