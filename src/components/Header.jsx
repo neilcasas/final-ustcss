216fca40cbb523715e2f0d597b1581d350334e63
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 02:29:35 +0800

    Added hiding navbar on scroll down with anims

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import cssLogo from "../assets/css.png";
 
 const Header = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (currentScrollPos > prevScrollPos) {
+        // Scrolling down, hide the header
+        setIsHidden(true);
+      } else {
+        // Scrolling up, show the header
+        setIsHidden(false);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
-    <header className="text-primary">
-      <div className="p-3 text-lg font-bold container mx-auto flex justify-between items-center">
-        <img className="css-logo h-[100px]" src={cssLogo} alt="css logo"></img>
-        <nav>
-          <ul className="flex space-x-4 gap-8">
-            <li>
-              <a href="">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Community</a>
-            </li>
-            <li>
-              <a href="/contact">News & Events</a>
-            </li>
-          </ul>
-        </nav>
+    <header
+      className={`text-primary sticky top-0 z-50 transition-all duration-300 ${
+        isHidden ? "transform -translate-y-full" : "transform translate-y-0"
+      }`}
+    >
+      <div className="p-1 text-lg font-bold bg-white shadow-lg">
+        <div className="container mx-auto flex justify-between items-center">
+          <img className="css-logo h-[80px]" src={cssLogo} alt="css logo"></img>
+          <nav>
+            <ul className="flex space-x-4 gap-8">
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Community</a>
+              </li>
+              <li>
+                <a href="/contact">News & Events</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
