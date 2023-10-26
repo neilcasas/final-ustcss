5bff4a7cb53b4e7791982df6da96f14747db18be
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-26 23:43:38 +0800

    Added feature which changes subjects based on specialization

--- src/components/Subjects.jsx
+++ src/components/Subjects.jsx
@@ -1,9 +1,17 @@
+import React, { useState } from "react";
 import Button from "./Button";
 import Subject from "./Subject";
+import subjectsData from "../../subjectsData.json";
 
 const subjectDescription = `Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut`;
 
 const Subjects = () => {
+  const [selectedCategory, setSelectedCategory] = useState("Data Science");
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div
       className="main-container flex flex-col items-center justify-center my-[100px]"
@@ -13,18 +21,28 @@ const Subjects = () => {
         <h3>Subjects</h3>
         <h6 className="w-[524px] mb-6">{subjectDescription}</h6>
         <div className="button-container flex justify-center gap-[15px]">
-          <Button text="Data Science" />
-          <Button text="Core Science" />
-          <Button text="Game Development" />
+          <Button
+            text="Data Science"
+            onClick={() => handleCategoryChange("Data Science")}
+          />
+          <Button
+            text="Core Science"
+            onClick={() => handleCategoryChange("Core Science")}
+          />
+          <Button
+            text="Game Development"
+            onClick={() => handleCategoryChange("Game Development")}
+          />
         </div>
       </div>
       <div className="subjects-container grid grid-cols-3 gap-[40px]">
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
-        <Subject subjectCode="CS26111" subjectName="Theory of Automata" />
+        {subjectsData[selectedCategory].map((subject, index) => (
+          <Subject
+            key={index}
+            subjectCode={subject.subjectCode}
+            subjectName={subject.subjectName}
+          />
+        ))}
       </div>
     </div>
   );
