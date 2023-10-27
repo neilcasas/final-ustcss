ae9126be4e051929d06478eb28fe448fd05823cb
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 21:12:27 +0800

    Adjusted text padding

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ const Post = (props) => {
         src={placeholder}
         alt="Post"
       />
-      <div className="absolute bottom-0 left-0 w-full flex flex-col justify-end bg-gradient-to-t from-black via-transparent to-transparent group-hover:scale-110 transition-transform">
+      <div className="absolute bottom-0 left-0 w-full flex flex-col justify-end bg-gradient-to-t from-black via-transparent to-transparent group-hover:scale-110 transition-transform p-4">
         <p className="text-white">{props.postTitle}</p>
         <p className="text-white">{props.date}</p>
       </div>
