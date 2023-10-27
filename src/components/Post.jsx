3045cd48ab475c4ae7b0808e755be9ec951566d0
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 21:11:08 +0800

    Added hover on carousel posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,9 +3,13 @@ import placeholder from "../assets/placeholder.png";
 
 const Post = (props) => {
   return (
-    <div className="relative">
-      <img className="h-[315px] w-[315px]" src={placeholder} alt="Post" />
-      <div className="absolute bottom-0 left-0 w-full flex flex-col justify-end">
+    <div className="relative group">
+      <img
+        className="h-[315px] w-[315px] group-hover:scale-110 transition-transform"
+        src={placeholder}
+        alt="Post"
+      />
+      <div className="absolute bottom-0 left-0 w-full flex flex-col justify-end bg-gradient-to-t from-black via-transparent to-transparent group-hover:scale-110 transition-transform">
         <p className="text-white">{props.postTitle}</p>
         <p className="text-white">{props.date}</p>
       </div>
