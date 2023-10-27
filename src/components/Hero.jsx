60a747e0b4aab7c30d6083b800bdf3dd8132dc8e
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 23:52:56 +0800

    Added text to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,11 +6,9 @@ const Hero = () => {
   return (
     <div className="text-center flex flex-col items-center justify-center h-screen">
       <h2 className="text-4xl leading-relaxed md: text-l sm:text-[66px] max-w-4xl">
-        Lorem ipsum dolor sit amet, consectetur adi
+        Meet the UST Computer Science Society
       </h2>
-      <h6 className="text-base sm:text-lg mt-2">
-        incididunt ut labore et dolore magna aliqua
-      </h6>
+      <h6 className="text-base sm:text-lg mt-2">Connect. Develop. Design.</h6>
       <div className="button-container flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 items-center justify-center mt-6">
         <Link to="tracks" smooth={true} offset={-180} duration={1000}>
           <Button text="#tracks" />
