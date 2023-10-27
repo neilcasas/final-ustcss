4eb81a2e3349c5a0a3f3146ef9b0d2b998f76a73
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-27 20:26:58 +0800

    Made hero component more mobile friendly

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,15 @@ import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-[713px] h-[170px] mx-auto my-48">
+    <div className="flex flex-col items-center justify-center w-full mx-auto my-6 px-4 sm:px-8">
       <div className="text-center">
-        <h2>Lorem ipsum dolor sit amet, consectetur adi</h2>
-        <h6>incididunt ut labore et dolore magna aliqua</h6>
-        <div className="button-container flex space-x-2 items-center justify-center my-10">
+        <h2 className="text-2xl sm:text-3xl">
+          Lorem ipsum dolor sit amet, consectetur adi
+        </h2>
+        <h6 className="text-sm sm:text-base">
+          incididunt ut labore et dolore magna aliqua
+        </h6>
+        <div className="button-container flex flex-col space-y-2 items-center justify-center mt-6">
           <Link to="tracks" smooth={true} offset={-200} duration={1000}>
             <Button text="#tracks" />
           </Link>
