219b5489fd41072499b910bf5aec9a8895d5a974
Neil Casas <neilalfonzcasas@gmail.com>
2023-10-29 13:09:59 +0800

    Fixed offset on scroll to track component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       </h2>
       <h6 className="text-base sm:text-lg mt-2">Connect. Develop. Design.</h6>
       <div className="button-container flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 items-center justify-center mt-6">
-        <Link to="tracks" smooth={true} offset={-180} duration={1000}>
+        <Link to="tracks" smooth={true} offset={-170} duration={1000}>
           <Button text="#tracks" />
         </Link>
         <Link to="subjects" smooth={true} duration={1000}>
